7898b4d89b1b5f16f77ce87ac0c0cf1b2b09f5b1
Fabian Lopez <65512494+fabianlopezar@users.noreply.github.com>
2026-02-08 11:41:26 -0500

    pg

--- src/db.js
+++ src/db.js
@@ -4,10 +4,17 @@ const fs = require('fs');
 const path = require('path');
 const {DB_USER, DB_PASSWORD, DB_HOST,} = process.env;
 
-const sequelize = new Sequelize(process.env.DATABASE_URL || `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:5432/postgres`, {
-  logging: false, // set to console.log to see the raw SQL queries
-  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-});
+const pg = require('pg');
+
+const sequelize = new Sequelize(
+  process.env.DATABASE_URL || `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:5432/postgres`,
+  {
+    dialect: 'postgres',
+    dialectModule: pg,
+    logging: false,
+  }
+);
+
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
